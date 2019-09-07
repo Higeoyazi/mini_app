efc0442d0735bf158dbc8d8290b33eec6966ae12
okamoto ryo <j270549a@gmail.com>
2019-09-07 22:50:10 +0900

    fix_header-layout
    ヘッダーを固定しました。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,7 @@ class TweetsController < ApplicationController
   # before_action :move_to_index, except: [:index, :show]
 
   def index
-    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    @tweets = Tweet.includes(:user).page(params[:page]).per(6).order("created_at DESC")
   end
 
   def new
