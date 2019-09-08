952dec1055e7012c6fdcb4e6459628fbc534a9c2
okamoto ryo <j270549a@gmail.com>
2019-09-09 03:27:19 +0900

    create_turbolinks

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,48 +1,50 @@
 // 非同期でコメント送信
-$(function(){
-  function buildHTML(comment){
-    var html = `<div class="comment" data-comment-id="${comment.id}">
-                  <strong>
-                    <a href=/users/${comment.user_id}>${comment.user_name}</a>
-                    ：
-                  </strong>
-                    ${comment.text}
-                  <li>
-                    <a href=/tweets/${comment.tweet_id}/comments/${comment.id}/edit class="ajax_edit_button">
-                      編集
-                    </a>
-                  </li>
-                  <li class="ajax_delete_button" data-tweet-id="${comment.tweet_id}" data-comment-id="${comment.id}" method="delete">
-                    削除
-                  </li>
-                </div>`
-    return html;
-  }
-  $('#new_comment').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(comment){
-      if (comment.text === ""){
-        alert('テキストを入力してください!');
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    function buildHTML(comment){
+      var html = `<div class="comment" data-comment-id="${comment.id}">
+                    <strong>
+                      <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                      ：
+                    </strong>
+                      ${comment.text}
+                    <li>
+                      <a href=/tweets/${comment.tweet_id}/comments/${comment.id}/edit class="ajax_edit_button">
+                        編集
+                      </a>
+                    </li>
+                    <li class="ajax_delete_button" data-tweet-id="${comment.tweet_id}" data-comment-id="${comment.id}" method="delete">
+                      削除
+                    </li>
+                  </div>`
+      return html;
+    }
+    $('#new_comment').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(comment){
+        if (comment.text === ""){
+          alert('テキストを入力してください!');
+          $('.btn').prop('disabled', false);
+        } else {
+        var html = buildHTML(comment);
+        $('.comments').append(html)
+        $("#new_comment")[0].reset();
         $('.btn').prop('disabled', false);
-      } else {
-      var html = buildHTML(comment);
-      $('.comments').append(html)
-      $("#new_comment")[0].reset();
-      $('.btn').prop('disabled', false);
-      $('.comments').animate({ scrollTop: $('.comments')[0].scrollHeight}, 'fast');
-      }
+        $('.comments').animate({ scrollTop: $('.comments')[0].scrollHeight}, 'fast');
+        }
+      })
     })
-  })
+  });
 });
 
 // 非同期でコメント削除
